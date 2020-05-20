7bd4195391758121aaf02a6c8a177a2ea5a6fc12
Jarosław Niesyto (jaronie711) <jaronie711@student.polsl.pl>
2020-05-20 02:18:39 +0200

    First interaction with GraphQL API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Menu from './Menu/Menu.js';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import {HashRouter, Route} from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import BrowseView from './Browse/BrowseView.js'
 import ProfileView from './Profile/ProfileView.js'
 import AddView from './Add/AddView.js'
 import LikedView from './Liked/LikedView.js'
-
-const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,20 +19,26 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const client = new ApolloClient({
+  uri: "https://how-do-i-gql.herokuapp.com/v1/graphql"
+});
+
 function App() {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-       <HashRouter>
-      <Menu></Menu>
-     
-        <Route path="/" exact/>
-        <Route path="/Browse" component={BrowseView}/>
-        <Route path="/Profile" component={ProfileView}/>
-        <Route path="/Liked" component={LikedView}/>
-        <Route path="/Add" component={AddView}/>
-      </HashRouter>
-    </div>
+    <ApolloProvider client={client}>
+      <div className={classes.root}>
+        <HashRouter>
+          <Menu></Menu>
+
+          <Route path="/" exact />
+          <Route path="/Browse" component={BrowseView} />
+          <Route path="/Profile" component={ProfileView} />
+          <Route path="/Liked" component={LikedView} />
+          <Route path="/Add" component={AddView} />
+        </HashRouter>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- src/Browse/BrowseView.js
+++ src/Browse/BrowseView.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag'
 
-export default function BrowseView(){
-    return(
-        <h1>browse</h1>
+export default function BrowseView() {
+    return (
+        <>
+            <Query query={gql` 
+                {
+                    recipes {
+                        author
+                        id
+                        ingredients
+                        name
+                        steps
+                    }  
+                }   
+                `} 
+            >
+
+                {({loading, error, data}) => {
+                    if(loading) return <p>loading</p>;
+                    if(error) return <p>error</p>;
+
+                    return(data.recipes.map(({author, id, ingredients, name, steps}) => (
+                      <div key={id}>
+                        {steps}
+                      </div>   
+                    )
+                    )
+
+                    )
+                }   
+                }
+            </Query>
+
+            <h1>browse</h1>
+        </>
     )
 }
